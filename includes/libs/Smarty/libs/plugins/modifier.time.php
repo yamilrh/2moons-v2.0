c34ffee7357b676d82c2b731c1207ab6c6d91c72
YamilRH <36506826+yamilrh@users.noreply.github.com>
2023-10-30 02:10:56 -0400

    showing the correct time by Smarty 4.3.2
    Este error provocaba que mostraba el tiempo de los edificios, investigaciones, naves, defensa, oficiales, bonos temporales de forma incorrecta

--- includes/libs/Smarty/libs/plugins/modifier.time.php
+++ includes/libs/Smarty/libs/plugins/modifier.time.php
@@ -15,5 +15,28 @@
  */
 function smarty_modifier_time($timeInSeconds)
 {
-    return date('H:i:s',$timeInSeconds);
+    global $LNG;
+
+    $day    = floor($timeInSeconds / 86400);
+    $hour   = floor((int)($timeInSeconds / 3600) % 24);
+    $minute = floor((int)($timeInSeconds / 60) % 60);
+    $second = floor((int) $timeInSeconds % 60);
+
+    $time  = '';
+
+    if($day > 0) {
+        $time .= sprintf('%d%s ', $day, $LNG['short_day']);
+    }
+
+    if($hour > 0) {
+        $time .= sprintf('%02d%s ', $hour, $LNG['short_hour']);
+    }
+
+    if($minute > 0) {
+        $time .= sprintf('%02d%s ', $minute, $LNG['short_minute']);
+    }
+
+    return $time.sprintf('%02d%s',
+        $second, $LNG['short_second']
+    );
 }
